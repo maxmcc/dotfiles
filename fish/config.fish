b092c1bee3c4cf0783df823cf279a88d2c7dfcfd
Max McCarthy <maxmcc@seas.upenn.edu>
2016-02-21 02:34:48 -0500

    Add autojump, remove broken brew plugins

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,13 @@
-fundle plugin 'tuvistavie/fish-completion-helpers'
+# Autojump {{{
+[ -f /usr/local/share/autojump/autojump.fish ];
+  and . /usr/local/share/autojump/autojump.fish
+# }}}
+
+# Fish plugins {{{
 fundle plugin 'tuvistavie/oh-my-fish-core'
+fundle plugin 'tuvistavie/fish-completion-helpers'
 fundle plugin 'oh-my-fish/theme-default'
+fundle plugin 'oh-my-fish/plugin-osx'
 
 fundle init
+# }}}
