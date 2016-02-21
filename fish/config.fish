69ce4dc81628d7e8351fa88bd0630db4a9e49451
Max McCarthy <maxmcc@seas.upenn.edu>
2016-02-21 02:43:29 -0500

    Add some helpful aliases and exports

--- fish/config.fish
+++ fish/config.fish
@@ -11,3 +11,28 @@ fundle plugin 'oh-my-fish/plugin-osx'
 
 fundle init
 # }}}
+
+# Aliases {{{
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias cp 'cp -v'
+alias mv 'mv -v'
+alias ll 'ls -FGlAhp'
+alias tns 'tmux new -s'
+alias tas 'tmux attach -t'
+alias tls 'tmux list-sessions'
+alias vim 'nvim'
+alias edit 'nvim'
+alias c 'clear'
+alias f 'open -a Finder .'
+alias gg 'git status'
+# }}}
+
+# Exports {{{
+set -gx PATH $HOME/.cabal/bin $PATH
+set -gx PATH $HOME/.multirust/toolchains/stable/cargo/bin $PATH
+set -gx PATH $HOME/.rbenv/bin $PATH
+set -gx PATH $HOME/.rbenv/shims $PATH
+set -gx PATH ./bin $PATH
+eval (opam config env)
+# }}}
