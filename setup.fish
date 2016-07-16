c472fcbf143485569ea01eeb01f60ae084ceebda
Max McCarthy <hello@maxmcc.com>
2016-07-16 13:28:22 -0700

    Include setup script

---
+++ setup.fish
@@ -0,0 +1,24 @@
+set DOTS ~/.dotfiles
+set CONFIG ~/.config
+
+mkdir -p $CONFIG/fish
+function fishlink
+    ln -s $DOTS/fish/$argv $CONFIG/fish/$argv
+end
+
+fishlink config.fish
+fishlink functions
+
+mkdir -p $CONFIG/nvim
+function vimlink
+    ln -s $DOTS/nvim/$argv $CONFIG/nvim/$argv
+end
+
+vimlink init.vim
+vimlink autoload
+vimlink ftplugin
+vimlink plugin
+
+ln -s $DOTS/tmux/tmux.conf ~/.tmux.conf
+ln -s $DOTS/git/config ~/.gitconfig
+
