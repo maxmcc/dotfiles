223e4c6e16206f10bfc9caf837548aa2eea9b034
Max McCarthy <max_mccarthy@apple.com>
2017-07-07 14:37:07 -0700

    Automatically install Fundle if it isn't installed already

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,11 @@
 # }}}
 
 # Fish plugins {{{
+if not functions -q fundle
+  mkdir -p ~/.config/fish/functions/
+  curl -fLo ~/.config/fish/functions/fundle.fish https://git.io/fundle
+end
+
 fundle plugin 'tuvistavie/oh-my-fish-core'
 fundle plugin 'tuvistavie/fish-completion-helpers'
 fundle plugin 'oh-my-fish/plugin-osx'
