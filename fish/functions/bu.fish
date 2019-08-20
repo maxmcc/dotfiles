a1becf2a9a5c2ea39ae43125f57f4449902ea43a
Max McCarthy <max_mccarthy@apple.com>
2019-08-20 09:57:06 -0700

    Add /usr/local/include path to brew chown command

--- fish/functions/bu.fish
+++ fish/functions/bu.fish
@@ -1,6 +1,8 @@
+# Defined in /var/folders/55/dtywpm7135s362j7140_vz140000gn/T//fish.7oKfNK/bu.fish @ line 2
 function bu
 	sudo chown -R (whoami) /usr/local/bin /usr/local/etc /usr/local/lib /usr/local/sbin \
-    /usr/local/share /usr/local/Frameworks /usr/local/share/locale /usr/local/share/man
+    /usr/local/share /usr/local/Frameworks /usr/local/share/locale /usr/local/share/man \
+    /usr/local/include
   and brew update
   and brew upgrade
   and brew cask upgrade
