49b5c8baf7558749aaf32226e0d5a5d2f778a752
Max McCarthy <max_mccarthy@apple.com>
2019-08-28 11:59:54 -0700

    Update setup function and include fish completions

--- setup.fish
+++ setup.fish
@@ -5,21 +5,27 @@ mkdir -p $CONFIG/fish
 function fishlink
     ln -Fs $DOTS/fish/$argv $CONFIG/fish/$argv
 end
-
 fishlink config.fish
 fishlink functions
+fishlink completions
 
 mkdir -p $CONFIG/nvim
 function vimlink
     ln -Fs $DOTS/nvim/$argv $CONFIG/nvim/$argv
 end
-
 vimlink init.vim
 vimlink ftplugin
 vimlink plugin
-
 curl -fLo $CONFIG/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -Fs $DOTS/tmux/tmux.conf ~/.tmux.conf
+
 ln -Fs $DOTS/git/config ~/.gitconfig
-mkdir -p ~/.ssh; and ln -Fs $DOTS/ssh/config ~/.ssh/config; and ln -Fs $DOTS/ssh/configs/ ~/.ssh/configs/
+
+mkdir -p ~/.ssh
+function sshlink
+  ln -Fs $DOTS/ssh/$argv ~/.ssh/$argv
+end
+sshlink config
+sshlink configs
+
