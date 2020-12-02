d6ce1286d5aba61cc1f158a2c07557c7b7f25ee6
Max McCarthy <hello@maxmcc.com>
2020-12-02 10:20:23 -0800

    Remove deprecated `brew cask upgrade` command

--- fish/functions/bu.fish
+++ fish/functions/bu.fish
@@ -1,7 +1,7 @@
-# Defined in /var/folders/fl/jnfk9c_16231nqf396jgs2gm0000gn/T//fish.zqHOLx/bu.fish @ line 2
+# Defined in /var/folders/fl/jnfk9c_16231nqf396jgs2gm0000gn/T//fish.wFSsIX/bu.fish @ line 2
 function bu
   brew update
   and brew upgrade
-  and brew cask upgrade
+  and brew upgrade --cask
   and brew cleanup
 end
