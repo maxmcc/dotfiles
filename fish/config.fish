7174dc6939e61286ae1234a4ec3a89a40a114381
Max McCarthy <hello@maxmcc.com>
2022-02-16 12:45:30 -0800

    Remove fundle

--- fish/config.fish
+++ fish/config.fish
@@ -3,18 +3,3 @@ if test -f (brew --prefix)/share/autojump/autojump.fish
   source (brew --prefix)/share/autojump/autojump.fish
 end
 # }}}
-
-# Fish plugins {{{
-if not functions -q fundle
-  mkdir -p ~/.config/fish/functions/
-  curl -fLo ~/.config/fish/functions/fundle.fish https://git.io/fundle
-end
-
-fundle plugin 'danhper/oh-my-fish-core'
-fundle plugin 'danhper/fish-completion-helpers'
-fundle plugin 'oh-my-fish/plugin-osx'
-fundle plugin 'danhper/fish-fastdir'
-
-fundle init
-# }}}
-
