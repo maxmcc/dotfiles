b5148e797093489ca51a05e1791d8351d4619ea1
Max McCarthy <hello@maxmcc.com>
2022-02-16 12:32:32 -0800

    Update to the right fundle plugins

--- fish/config.fish
+++ fish/config.fish
@@ -10,10 +10,10 @@ if not functions -q fundle
   curl -fLo ~/.config/fish/functions/fundle.fish https://git.io/fundle
 end
 
-fundle plugin 'tuvistavie/oh-my-fish-core'
-fundle plugin 'tuvistavie/fish-completion-helpers'
+fundle plugin 'danhper/oh-my-fish-core'
+fundle plugin 'danhper/fish-completion-helpers'
 fundle plugin 'oh-my-fish/plugin-osx'
-fundle plugin 'tuvistavie/fish-fastdir'
+fundle plugin 'danhper/fish-fastdir'
 
 fundle init
 # }}}
