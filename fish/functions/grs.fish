65203394599773412b55c645d476652a38159b71
Max McCarthy <max_mccarthy@apple.com>
2022-03-24 16:50:27 -0700

    Add gd (git diff) and grs (git restore --staged)

---
+++ fish/functions/grs.fish
@@ -0,0 +1,9 @@
+function grs
+    if not set -q argv[1]
+        echo 'git restore --staged .'
+        command git restore --staged .
+    else
+        echo "git restore --staged $argv"
+        command git restore --staged $argv
+    end
+end
