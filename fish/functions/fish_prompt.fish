587e30ac128010d5412dd1d99705a0ca85716ded
Max McCarthy <max_mccarthy@apple.com>
2023-03-05 09:38:18 -0800

    Update fish prompt characters

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -11,9 +11,9 @@ function fish_prompt
   set -l fish     "⋊>"
   set -l ahead    "↑"
   set -l behind   "↓"
-  set -l diverged ""
-  set -l dirty    "✗"
-  set -l none     "◦"
+  set -l diverged "△"
+  set -l dirty    "◆"
+  set -l none     "◇"
 
   set -l normal_color     (set_color normal)
   set -l success_color    (set_color cyan)
